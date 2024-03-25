4e638b4a9e7751358f77c1ef85d728eca6a5d74d
Alexandr Vinokurov <salispiligrim@yandex.ru>
2024-03-25 15:59:56 +0100

    add less3_4/homework:Task1

---
+++ functions/homework/Task1/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+    Напишите программу, которая бесконечно запрашивает целые числа с консоли. Программа завершается 
+    при вводе символа ‘q’ или при вводе числа, 
+    сумма цифр которого чётная.
+*/
+int GetSumNumbersDigit(int num)
+{
+    int sum = 0;
+    while (num >= 1)
+    {
+        sum += num % 10;
+        num = num / 10;
+    }
+    return sum;
+}
+
+void InputEvenNumber()
+{
+
+    while (true)
+    {
+        Console.WriteLine("Введите целое число, сумма цифр которого четная или введите 'q'");
+        string inputData = Console.ReadLine();
+        int sumNumber = 1;
+        int IsNumeric;
+        if (int.TryParse(inputData, out IsNumeric))
+        {
+            int inputNumber = Convert.ToInt32(inputData);
+            sumNumber = GetSumNumbersDigit(inputNumber);
+        }
+
+        if (inputData == "q" || sumNumber % 2 == 0)
+        {
+            Console.WriteLine("Программа завершена");
+            return;
+        }
+    }
+}
+
+InputEvenNumber();
