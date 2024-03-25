38e9633c8a5fdfe040b692ee791141676de7f1d6
Alexandr Vinokurov <salispiligrim@yandex.ru>
2024-03-25 16:55:04 +0100

    add less3_4/homework:Task3

---
+++ functions/homework/Task3/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+    Напишите программу, которая перевернёт одномерный массив 
+    (первый элемент станет последним, второй – предпоследним и т.д.)
+*/
+
+int[] GetArray(int size)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(1, 10);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("Массив: ");
+    foreach (int e in arr)
+    {
+        Console.Write($"{e} ");
+    }
+    Console.Write("\n");
+}
+
+int[] GetFlipArray(int[] array)
+{
+    int arrayEndIndex = array.Length - 1;
+    int[] flipArray = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        flipArray[i] = array[arrayEndIndex];
+        arrayEndIndex--;
+    }
+
+    return flipArray;
+}
+
+int sizeArray = 10;
+int[] arrayNumbers = GetArray(sizeArray);
+PrintArray(arrayNumbers);
+int[] flipArr = GetFlipArray(arrayNumbers);
+PrintArray(flipArr);
+
